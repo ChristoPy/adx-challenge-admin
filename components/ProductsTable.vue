231748673ddcc2fff10204c0c1696614773981a0
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-04-20 09:30:16 -0300

    Update ProductsTable.

--- components/ProductsTable.vue
+++ components/ProductsTable.vue
@@ -1,20 +1,14 @@
 <template>
-  <div>
-    <table class="table-auto w-full shadow-md">
-      <thead class="border">
-        <tr>
-          <th class="px-4 py-2 text-left">Nome</th>
-          <th class="px-4 py-2 text-left border">Preço</th>
-          <th class="px-4 py-2 text-left">Estoque</th>
-        </tr>
-      </thead>
-      <tbody>
-        <ProductTableItem
-          v-for="product of $store.getters['products/products']"
-          :key="product._id"
-          :product="product"
-        />
-      </tbody>
-    </table>
-  </div>
+  <table class="table-auto w-full shadow-md">
+    <thead class="border">
+      <tr>
+        <th class="px-4 py-2 text-left">Nome</th>
+        <th class="px-4 py-2 text-left border">Preço</th>
+        <th class="px-4 py-2 text-left">Estoque</th>
+      </tr>
+    </thead>
+    <tbody>
+      <ProductTableItem v-for="product of $store.getters['products/products']" :key="product._id" :product="product" />
+    </tbody>
+  </table>
 </template>
